56067eb1e9f6048dd981cfa639b89fec7fd8765d
lusitaniae <pedrogomesyoo@gmail.com>
2017-04-11 00:21:38 +0200

    Reintroduce Fuel ORM into the benchmark
    Updated fuel from 1.7 to 1.8\n
    Included dependencies on the repo\n
    Added to the database configuration file\n
    'charset'    => false\n
    so that the charset is not forced into the connection, which will throw an exception.

---
+++ fuel_18/fuel/app/config/development/db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+	'default' => array(
+		'connection'  => array(
+//			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+            'dsn'        => 'sqlite::memory:',
+			'username'   => '',
+			'password'   => '',
+		),
+		'charset'    => false
+	),
+);
---
+++ fuel_18/fuel/app/logs/2017/04/10.php
@@ -0,0 +1,42 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2017-04-10 23:10:42 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:10:43 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:11:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:12:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:12:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:13:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:13:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:17:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-04-10 23:24:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:24:58 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:24:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:24:58 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:25:30 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:25:30 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:25:30 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:25:30 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:41:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:41:17 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:41:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:41:17 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:41:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:41:55 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:41:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:41:55 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:42:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:42:11 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:42:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:42:11 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:56:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:56:01 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:56:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:56:01 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:57:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:57:37 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:57:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:57:37 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:58:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:58:52 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
+WARNING - 2017-04-10 23:58:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2017-04-10 23:58:52 --> Fatal Error - SQLSTATE[HY000]: General error: 1 near "SET": syntax error in /home/icarus/forked-php-orm-benchmark/fuel_173/fuel/core/classes/database/pdo/connection.php on line 109
